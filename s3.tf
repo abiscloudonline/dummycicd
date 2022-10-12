642273c52c727b67cc0a5cdd82d06641dce37267
root <root@ip-172-31-39-242.ap-south-1.compute.internal>
2022-10-12 06:22:26 +0000

    s3 changed

--- s3.tf
+++ s3.tf
@@ -19,16 +19,6 @@ resource "aws_s3_bucket" "vpctestbucket" {
   }
 }
 
-resource "aws_dynamodb_table" "state_locking" {
-  hash_key = "LockID"
-  name     = "dynamodb-state-locking"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  billing_mode = "PAY_PER_REQUEST"
-}
-
 terraform {
     backend "s3" {
         bucket = "terraform-tfstate-s3-bucket"
