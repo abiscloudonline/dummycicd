c31c93d3097ec2305a4af46f9026de7b8f2e862d
root <root@ip-172-31-39-242.ap-south-1.compute.internal>
2022-10-12 06:03:37 +0000

    added remote storage of tfstate file

--- sample_s3.tf
+++ sample_s3.tf
@@ -18,3 +18,22 @@ resource "aws_s3_bucket" "vpctestbucket" {
     Environment = "VPC_Endpoint_test"
   }
 }
+
+resource "aws_dynamodb_table" "state_locking" {
+  hash_key = "LockID"
+  name     = "dynamodb-state-locking"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
+
+terraform {
+    backend "s3" {
+        bucket = "terraform-tfstate-s3-bucket"
+        key    = "terraform/remote/s3/terraform.tfstate"
+        region     = "ap-south-1"
+       dynamodb_table  = "dynamodb-state-locking"
+    }
+}
